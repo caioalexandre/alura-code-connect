cc0d11520d71c6b333a674dab4030ca5ebedcf59
Caio Oliveira <caioalexandretkd@hotmail.com>
2024-07-17 10:47:21 -0300

    - created Asided component - imported logo from Code Connect - styled some components

---
+++ src/components/Aside/index.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from './aside.module.css'
+import logo from './logo.png'
+
+export const Aside = () => {
+	return (
+		<aside className={styles.aside}>
+			<Image
+				src={logo}
+				alt="Code Connect Logo"
+			/>
+		</aside>
+	)
+};
