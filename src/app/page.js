531d705f648ed1d7442cf815d3805065c3e5b829
Caio Oliveira <caioalexandretkd@hotmail.com>
2024-07-25 08:43:25 -0300

    - paginated API request

--- src/app/page.js
+++ src/app/page.js
@@ -2,23 +2,8 @@ import { CardPost } from "@/components/CardPost";
 import logger from "@/logger";
 import styles from "./page.module.css"
 
-// const post = {
-//   "id": 1,
-//   "cover": "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/posts/introducao-ao-react.png",
-//   "title": "Introdução ao React",
-//   "slug": "introducao-ao-react",
-//   "body": "Neste post, vamos explorar os conceitos básicos do React, uma biblioteca JavaScript para construir interfaces de usuário. Vamos cobrir componentes, JSX e estados.",
-//   "markdown": "```javascript\nfunction HelloComponent() {\n  return <h1>Hello, world!</h1>;\n}\n```",
-//   "author": {
-//       "id": 101,
-//       "name": "Ana Beatriz",
-//       "username": "anabeatriz_dev",
-//       "avatar": "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/authors/anabeatriz_dev.png"
-//   }
-// };
-
-async function getPosts() {
-  const response = await fetch('http://localhost:3042/posts');
+async function getPosts(page) {
+  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`);
 
   if (!response.ok) {
     logger.error('Woops, something went wrong! page.js@getPosts');
@@ -32,11 +17,11 @@ async function getPosts() {
 };
 
 export default async function Home() {
-  const posts = await getPosts();
+  const { data: posts } = await getPosts(1);
 
   return (
     <main className={styles.grid}>
-      {posts.map(post => <CardPost post={post} />)}
+      {posts.map(post => <CardPost key={post.id} post={post} />)}
     </main>
   );
 }
