f0002ff04c007f875621922c232143c1a6b9735a
Caio Oliveira <caioalexandretkd@hotmail.com>
2024-07-25 08:52:04 -0300

    - creating pagination controls

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import { CardPost } from "@/components/CardPost";
 import logger from "@/logger";
 import styles from "./page.module.css"
+import Link from "next/link";
 
 async function getPosts(page) {
   const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`);
@@ -17,11 +18,14 @@ async function getPosts(page) {
 };
 
 export default async function Home() {
-  const { data: posts } = await getPosts(1);
+  const { data: posts, prev, next } = await getPosts(1);
 
   return (
     <main className={styles.grid}>
       {posts.map(post => <CardPost key={post.id} post={post} />)}
+
+      {prev && <Link href={`/?page=${prev}`}>Previous Page</Link>}
+      {next && <Link href={`/?page=${next}`}>Next Page</Link>}
     </main>
   );
 }
