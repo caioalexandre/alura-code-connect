68b464171ae8fae3a1e4fabb0e91b2a86dd47ad6
Caio Oliveira <caioalexandretkd@hotmail.com>
2024-07-25 09:20:56 -0300

    - styled pagination links - made params from route dynamically

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css"
 import Link from "next/link";
 
 async function getPosts(page) {
-  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`);
+  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=4`);
 
   if (!response.ok) {
     logger.error('Woops, something went wrong! page.js@getPosts');
@@ -17,15 +17,19 @@ async function getPosts(page) {
   return response.json();
 };
 
-export default async function Home() {
-  const { data: posts, prev, next } = await getPosts(1);
+export default async function Home({ searchParams }) {
+  const currentPage = searchParams?.page || 1;
+  
+  const { data: posts, prev, next } = await getPosts(currentPage);
 
   return (
     <main className={styles.grid}>
       {posts.map(post => <CardPost key={post.id} post={post} />)}
 
-      {prev && <Link href={`/?page=${prev}`}>Previous Page</Link>}
-      {next && <Link href={`/?page=${next}`}>Next Page</Link>}
+      <div className={styles.links}>
+        {prev && <Link href={`/?page=${prev}`}>{'<'} Previous Page</Link>}
+        {next && <Link href={`/?page=${next}`}>Next Page {'>'}</Link>}
+      </div>
     </main>
   );
 }
